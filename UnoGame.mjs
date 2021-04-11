aaff835de5bb324c73e326b9053ac07f60a16a27
raquelrius <raquelrius@hotmail.com>
2021-04-11 18:44:58 +0200

    feat: added new methods for game class

--- UnoGame.mjs
+++ UnoGame.mjs
@@ -1,6 +1,6 @@
 const colors = ['blue', 'green', 'red', 'yellow'];
 
-const behaviour = {
+const typeCard = {
     0: 'number',
     1: 'number',
     2: 'number',
@@ -17,77 +17,110 @@ const behaviour = {
     13: 'skip',
     14: 'reverse',
 }
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
 }
 
-function createAllCards(totalCards) {
+const createAllCards = () => {
     const cards = [];
-    Object.entries(behaviour).forEach(([key, value]) => {
-        console.log(key,value)
+    Object.entries(typeCard).forEach(([key, value]) => {
         if (!value.includes('wild')) {
             colors.forEach(color => {
-                const card = new Card(color, value);
+                const card = new Card(color, key, value);
                 cards.push(card);
             })
             if (key.toString() !== '0') {
-                console.log(key)
                 colors.forEach(color => {
-                    const card = new Card(color, value);
+                    const card = new Card(color, key, value);
                     cards.push(card);
                 })
             }
+        } else {
+            cards.push(new Card('none', key, value));
+            cards.push(new Card('none', key, value));
+            cards.push(new Card('none', key, value));
+            cards.push(new Card('none', key, value));
         }
-        cards.push(new Card('none', value));
-        cards.push(new Card('none', value));
-        cards.push(new Card('none', value));
-        cards.push(new Card('none', value));
     })
     return cards;
 }
 
 class Game {
     constructor() {
-        this.totalCards = 180;
+        this.deck = shuffle(createAllCards());
+        this.discardPile = [];
         this.players = {};
-        this.cards = createAllCards(180);
     }
 
-    addPlayers(name) {
+    addPlayer(name) {
         this.players[name] = new Player();
     }
-    dealCards() {
-        this.players.forEach(player => {
-            const color = colors[getRandomInt(0,4)];
-            const number = getRandomInt(0,15);
-            player.hand = new Hand();
 
+    addPlayers(namesList) {
+        namesList.forEach(name => this.players[name] = new Player());
+    }
+
+    dealCards() {
+        Object.keys(this.players).forEach(player => {
+            const hand = new Hand();
+            for (let i = 0; i < 7; i++) { // each player has 7 cards
+                const drawnCard = this.deck.pop();
+                hand.addCard(drawnCard);
+            }
+            this.players[player].setHand(hand);
         })
+    }
+
+    playerDrawCards(player, amountCards) {
+        const totalCards = [];
+        for (let i = 0; i < amountCards; i++) {
+            if (this.deck.length !== 0) {
+                const drawnCard = this.deck.pop();
+                this.players[player]?.hand.addCard(drawnCard);
+            } else {
+                this.deck = shuffle(this.deck.concat(this.discardPile)); // if there are no more cards, we add the discard pile
+            }
+        }
+        return totalCards;
+    }
+
+    playerPLayCard(player, card) {
+
     }
 }
 
 class Card {
-    constructor(color, number) {
+    constructor(color, number, behaviour) {
         this.color = color;
         this.number = number;
+        this.behaviour = behaviour;
     }
 }
 
 class Hand {
-    constructor(cards) {
-        this.cards = cards;
+    constructor() {
+        this.cards = [];
     }
 
     addCard(card) {
-        this.cards.add(card);
+        this.cards.push(card);
     }
-
 }
 
 class Player {
     constructor() {
-        this.hand = undefined;
+        this.hand = new Hand();
     }
+
+    setHand(hand) {
+        this.hand = hand;
+    }
+
 }
 
 
